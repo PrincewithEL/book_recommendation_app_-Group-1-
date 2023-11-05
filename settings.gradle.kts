11c97d10a3f492a129a6f8e904cabf9fffd07ddc
PrincewithEL <86285043+PrincewithEL@users.noreply.github.com>
2023-11-05 21:20:22 +0300

    Book Recommendations App

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "book_recommendations_app_(Group 1)"
+include(":app")
